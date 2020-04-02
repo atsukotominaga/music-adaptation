89e9191a02c33031df88c325276d93b0fa1cc1bc
Atsuko Tominaga <Tominaga_Atsuko@phd.ceu.edu>
2020-04-02 12:01:08 +0200

    add preprosessor for stim_n
    Signed-off-by: Atsuko Tominaga <Tominaga_Atsuko@phd.ceu.edu>

---
+++ material/filtering.R
@@ -0,0 +1,105 @@
+#!/usr/local/bin/R
+#rm(list=ls(all=T)) - clear all in Grobal Environment
+
+####################################
+#  Documentation
+####################################
+# Created: 02/04/2020
+# This script organises raw data and removes pitch errors.
+# GitHub repo (private): https://github.com/atsukotominaga/teaching-v2.0/tree/master/script/R 
+
+####################################
+#  Requirements
+####################################
+# set working directory to file source location
+# install and load required packages
+if (!require("data.table")) {install.packages("data.table"); require("data.table")}
+
+# read functions
+source("./function.R")
+
+# create necessary folders if not exist
+# filtered - all of the outputs will be stored in this folder
+if (!file.exists("filtered")){
+  dir.create("filtered")
+}
+
+# read a text file for ideal performance
+df_ideal <- fread("./ideal.txt")
+colnames(df_ideal) <- "Pitch"
+df_ideal$RowNr <- c(1:nrow(df_ideal))
+
+# create a list of data file names
+lf <- list.files("./practice", pattern = "txt")
+
+# create raw_data - merge all data files into one
+raw_data <- data.table()
+for (i in 1:length(lf)){
+  data_i <- read.csv(file.path("./practice", lf[i]), header = F, sep = " ", dec = ".")
+  raw_data <- rbind(raw_data, data_i)
+}
+
+# add column namesls
+colnames(raw_data) <- c("NoteNr", "TimeStamp", "Pitch", "Velocity", "Key_OnOff", "Device", "Tempo",
+                        "SubNr", "BlockNr", "TrialNr", "Skill", "Condition", "Image")
+
+# clean raw_data
+raw_data$NoteNr <- as.numeric(gsub(",", "", raw_data$NoteNr))
+raw_data$Image <- gsub(";", "", raw_data$Image)
+
+# sort by SubNr, BlockNr, TrialNr
+raw_data <- raw_data[order(raw_data$SubNr, raw_data$BlockNr, raw_data$TrialNr),]
+
+# separate raw_data into each skill
+df_stim_n <- raw_data[BlockNr == 0] # non-expressive sheet music
+df_stim_a <- raw_data[Skill == "articulation"]
+df_stim_d <- raw_data[Skill == "dynamics"]
+
+####################################
+# Detect pitch errors
+####################################
+# raw_data without metronome
+df_all <- df_stim_n[Pitch != 31 & Pitch != 34]
+
+# onset and offset
+df_onset <- df_all[Key_OnOff == 1]
+df_offset <- df_all[Key_OnOff == 0]
+
+####################################
+# ONSET
+####################################
+# detect pitch errors
+ls_error_onset  <- pitch_remover(df_onset, df_ideal)
+
+# mark trials with errors by the first filtering
+df_onset$Error <- 0
+for (error in 1:length(ls_error_onset)){
+  df_onset[SubNr == ls_error_onset[[error]][1] & TrialNr == ls_error_onset[[error]][2]]$Error <- 1
+}
+
+df_correct_onset <- df_onset[Error == 0]
+
+####################################
+# OFFSET
+####################################
+# detect pitch errors
+ls_error_offset  <- pitch_remover(df_offset, df_ideal)
+
+# mark trials with errors by the first filtering
+df_offset$Error <- 0
+for (error in 1:length(ls_error_offset)){
+  df_offset[SubNr == ls_error_offset[[error]][1] & TrialNr == ls_error_offset[[error]][2]]$Error <- 1
+}
+
+df_correct_offset <- df_offset[Error == 0]
+
+####################################
+# Export csv files
+####################################
+# Export corrected onset/offset for stim_n
+write.csv(df_correct_onset, file = "./filtered/data_onset.csv", row.names = F)
+write.csv(df_correct_offset, file = "./filtered/data_offset.csv", row.names = F)
+
+# check whether there is still error
+pitch_remover(df_correct_onset, df_ideal)
+pitch_remover(df_correct_offset, df_ideal)
---
+++ material/function.R
@@ -0,0 +1,40 @@
+##### FUNCTIONS #####
+
+# install and load required packages
+if (!require("dplyr")) {install.packages("dplyr"); require("dplyr")}
+if (!require("data.table")) {install.packages("data.table"); require("data.table")}
+
+### define pitch remove function (pitch_remover)
+# data: data of all trials (df_onset/df_offset)
+# ideal: df_ideal
+pitch_remover <- function(data, ideal){
+  ls_removed <- list()
+  for (subnr in unique(data$SubNr)){ #set # of participants
+    print(sprintf("---SubNr %i---", subnr))
+    for (trial in unique(unique(data[SubNr == subnr]$TrialNr))){
+      current <- data %>% dplyr::filter(SubNr == subnr &  TrialNr == trial)
+      if (nrow(current) != 0){ #if current data is not empty
+        if (length(ideal$Pitch) != length(current$Pitch)){ #if # of onsets/offsets is not equal to ideal performance
+          ls_removed <- c(ls_removed, list(c(subnr, trial, "NoteNr error")))
+          print(sprintf("NoteNr error - SubNr/TrialNr: %i/%i", subnr, trial))
+        } else if (length(ideal$Pitch) == length(current$Pitch)) { #if # of onsets and offsets are correct
+          counter = 0 #set a counter so that the following loop will terminate once it detects one pitch error in a trial
+          for (note in 1:length(ideal$Pitch)){
+            # detect onset error
+            if (current$Pitch[note] != ideal$Pitch[note]){
+              while (counter == 0){
+                ls_removed <- c(ls_removed, list(c(subnr, trial, "Pitch error")))
+                print(sprintf("Pitch error - SubNr/TrialNr/NoteNr: %i//%i/%i", subnr, trial, note))
+                counter = counter + 1
+              }
+            }
+          }
+        }
+      } else { #if current data is emtpy"
+        ls_removed <- c(ls_removed, list(c(subnr, trial, "Missing")))
+        print(sprintf("Missing - SubNr/TrialNr: %i/%i", subnr, trial))
+      }
+    }
+  }
+  return(ls_removed)
+}
