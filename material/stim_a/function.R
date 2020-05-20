c93c25865611f1539b82159788bd0986102a4f7e
Atsuko Tominaga <Tominaga_Atsuko@phd.ceu.edu>
2020-05-20 09:25:39 +0200

    update function.R for averaging three performances
    Signed-off-by: Atsuko Tominaga <Tominaga_Atsuko@phd.ceu.edu>

--- material/stim_a/function.R
+++ material/stim_a/function.R
@@ -1,4 +1,4 @@
-# averaging function (create 6 instances)
+# averaging function (create 8 instances - averaging 3 performances)
 # packages
 if (!require("data.table")) {install.packages("data.table"); require("data.table")}
 
@@ -9,20 +9,14 @@ combining_onset <- function(valid, number){
   SubNr1 <- valid[Sample == number+counter]$SubNr
   SubNr2 <- valid[Sample == number+1+counter]$SubNr
   SubNr3 <- valid[Sample == number+2+counter]$SubNr
-  SubNr4 <- valid[Sample == number+3+counter]$SubNr
-  SubNr5 <- valid[Sample == number+4+counter]$SubNr
   TrialNr1 <- valid[Sample == number+counter]$TrialNr
   TrialNr2 <- valid[Sample == number+1+counter]$TrialNr
   TrialNr3 <- valid[Sample == number+2+counter]$TrialNr
-  TrialNr4 <- valid[Sample == number+3+counter]$TrialNr
-  TrialNr5 <- valid[Sample == number+4+counter]$TrialNr
   stim_1 <- dt_onset[SubNr == SubNr1 & TrialNr == TrialNr1]
   stim_2 <- dt_onset[SubNr == SubNr2 & TrialNr == TrialNr2]
   stim_3 <- dt_onset[SubNr == SubNr3 & TrialNr == TrialNr3]
-  stim_4 <- dt_onset[SubNr == SubNr4 & TrialNr == TrialNr4]
-  stim_5 <- dt_onset[SubNr == SubNr5 & TrialNr == TrialNr5]
-  outcome <- rbind(stim_1, stim_2, stim_3, stim_4, stim_5)
-  outcome$RowNr <- rep(c(1:72), 5)
+  outcome <- rbind(stim_1, stim_2, stim_3)
+  outcome$RowNr <- rep(c(1:72), 3)
   
   # tempo labels
   outcome[Tempo == 120]$Tempo <- 250
@@ -35,20 +29,14 @@ combining_offset <- function(valid, number){
   SubNr1 <- valid[Sample == number+counter]$SubNr
   SubNr2 <- valid[Sample == number+1+counter]$SubNr
   SubNr3 <- valid[Sample == number+2+counter]$SubNr
-  SubNr4 <- valid[Sample == number+3+counter]$SubNr
-  SubNr5 <- valid[Sample == number+4+counter]$SubNr
   TrialNr1 <- valid[Sample == number+counter]$TrialNr
   TrialNr2 <- valid[Sample == number+1+counter]$TrialNr
   TrialNr3 <- valid[Sample == number+2+counter]$TrialNr
-  TrialNr4 <- valid[Sample == number+3+counter]$TrialNr
-  TrialNr5 <- valid[Sample == number+4+counter]$TrialNr
   stim_1 <- dt_offset[SubNr == SubNr1 & TrialNr == TrialNr1]
   stim_2 <- dt_offset[SubNr == SubNr2 & TrialNr == TrialNr2]
   stim_3 <- dt_offset[SubNr == SubNr3 & TrialNr == TrialNr3]
-  stim_4 <- dt_offset[SubNr == SubNr4 & TrialNr == TrialNr4]
-  stim_5 <- dt_offset[SubNr == SubNr5 & TrialNr == TrialNr5]
-  outcome <- rbind(stim_1, stim_2, stim_3, stim_4, stim_5)
-  outcome$RowNr <- rep(c(1:72), 5)
+  outcome <- rbind(stim_1, stim_2, stim_3)
+  outcome$RowNr <- rep(c(1:72), 3)
   
   # tempo labels
   outcome[Tempo == 120]$Tempo <- 250
