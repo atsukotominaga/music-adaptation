80d4ecee3d14dd5effd785903ebbac21b616a12e
Atsuko Tominaga <Tominaga_Atsuko@phd.ceu.edu>
2020-05-22 14:17:32 +0200

    implemeting automatic saving function for midi TBC
    Signed-off-by: Atsuko Tominaga <Tominaga_Atsuko@phd.ceu.edu>

--- material/playback.py
+++ material/playback.py
@@ -11,35 +11,49 @@ import numpy as np
 #%% open port (play sounds with Max MSP)
 port = mido.open_output('to Max 1')
 
-#　%% playback
-with open("./stim_d/1589959883-200520/3_instance.txt") as csvfile:
-    current = csv.reader(csvfile, delimiter = ",")
-    next(current) # skip first row
-    counter = 0
-    for row in current:
-        if counter == 0:
-            previousTime = 0
-            counter += 1
-        else:
-            previousTime = currentTime
-        currentTime = int(row[3])*0.001
-        currentPitch = int(row[5])
-        currentVelocity = int(row[4])
-        currentOnOff = int(row[2])
-        # assign midi values
-        if currentOnOff == 1:
-            print(currentOnOff)
-            msg = mido.Message('note_on', note = currentPitch, velocity = currentVelocity, time = currentTime)
-            print(msg)
-            time.sleep(currentTime-previousTime)
-            print(msg.time)
-            port.send(msg)
-        elif currentOnOff == 0:
-            msg = mido.Message('note_off', note = currentPitch, velocity = currentVelocity, time = currentTime)
-            print(msg)
-            time.sleep(currentTime-previousTime)
-            print(msg.time)
-            port.send(msg)
+# %% read files in a directory
+files = [f for f in os.listdir("./stim_n/20_21/1589957466-200520") if not f.startswith("dt_") and not f.startswith("valid")]
+
+#　%% playback and save mid.file
+
+for file in files:
+    with open("./stim_d/1589959883-200520/"+file) as csvfile:
+        current = csv.reader(csvfile, delimiter = ",")
+        next(current) # skip first row
+        mid = mido.MidiFile()
+        track = mido.MidiTrack()
+        mid.tracks.append(track)
+        track.append(mido.Message('program_change', program=12, time=0))
+
+        counter = 0
+        for row in current:
+            if counter == 0:
+                previousTime = 0
+                counter += 1
+            else:
+                previousTime = currentTime
+            currentTime = int(row[3])*0.001
+            currentPitch = int(row[5])
+            currentVelocity = int(row[4])
+            currentOnOff = int(row[2])
+            # assign midi values
+            if currentOnOff == 1:
+                print(currentOnOff)
+                msg = mido.Message('note_on', note = currentPitch, velocity = currentVelocity, time = currentTime)
+                track.append(msg)
+                print(msg)
+                time.sleep(currentTime-previousTime)
+                print(msg.time)
+                port.send(msg)
+            elif currentOnOff == 0:
+                msg = mido.Message('note_off', note = currentPitch, velocity = currentVelocity, time = currentTime)
+                track.append(msg)
+                print(msg)
+                time.sleep(currentTime-previousTime)
+                print(msg.time)
+                port.send(msg)
+# %% save mid file
+mid.save('new_song.mid')
 
 
 # %%
