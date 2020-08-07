0a852d771bcd432370548d55f126adc186bd35b2
Atsuko Tominaga <Tominaga_Atsuko@phd.ceu.edu>
2020-08-07 23:27:24 +0200

    implement mid export
    Signed-off-by: Atsuko Tominaga <Tominaga_Atsuko@phd.ceu.edu>

--- material/stimuli/playback.py
+++ material/stimuli/playback.py
@@ -23,8 +23,22 @@ folders = [folder_low, folder_art, folder_dyn, folder_high]
 #%% playback
 for folder in folders:
     for instance in range(1, 17):
-        filename = folder + instance + "_instance.txt"
+        filename = folder + str(instance) + "_instance.txt"
         with open(filename) as csvfile:
+            mid = mido.MidiFile()
+            track = mido.MidiTrack()
+            mid.tracks.append(track)
+            if folder == "./low/1596207520-310720/":
+                midname = "./mid/low_" + str(instance) + ".mid"
+            elif folder == "./art/1596379937-020820/":
+                midname = "./mid/art_" + str(instance) + ".mid"
+            elif folder == "./dyn/1596379899-020820/":
+                midname = "./mid/dyn_" + str(instance) + ".mid"
+            elif folder == "./high/1596380946-020820/":
+                midname = "./mid/high_" + str(instance) + ".mid"
+
+            track.append(mido.Message('program_change', program=12, time=0))
+
             current = csv.reader(csvfile, delimiter = ",")
             next(current) # skip first row
             counter = 0
@@ -34,61 +48,19 @@ for folder in folders:
                     counter += 1
                 else:
                     previousTime = currentTime
-                currentTime = int(row[3])*0.001+3 # 3 sec delay at the beginning
+                currentTime = int(mido.second2tick(int(row[3])*0.001+3, 480, 500000))
+                print(currentTime-previousTime)
                 currentPitch = int(row[5])
                 currentVelocity = int(row[4])
                 currentOnOff = int(row[2])
                 # assign midi values
                 if currentOnOff == 1:
-                    print(currentOnOff)
-                    msg = mido.Message('note_on', note = currentPitch, velocity = currentVelocity, time = currentTime)
-                    print(msg)
-                    time.sleep(currentTime-previousTime)
-                    print(msg.time)
-                    port.send(msg)
+                    #print(currentOnOff)
+                    track.append(mido.Message('note_on', note=currentPitch, velocity=currentVelocity, time=currentTime-previousTime))
                 elif currentOnOff == 0:
-                    msg = mido.Message('note_off', note = currentPitch, velocity = currentVelocity, time = currentTime)
-                    print(msg)
-                    time.sleep(currentTime-previousTime)
-                    print(msg.time)
-                    port.send(msg)
-                time.sleep(3) # 3 sec pause
-
-#%%
-with open("./low/1596207520-310720/1_instance.txt") as csvfile:
-    current = csv.reader(csvfile, delimiter = ",")
-    next(current) # skip first row
-    msg = mido.Message('note_on', note = 64, velocity = 1) # switch ON "record" on Max/MSP
-    port.send(msg)
-    counter = 0
-    for row in current:
-        if counter == 0:
-            previousTime = 0
-            counter += 1
-        else:
-            previousTime = currentTime
-        currentTime = int(row[3])*0.001+3 # 3 sec delay at the beginning
-        currentPitch = int(row[5])
-        currentVelocity = int(row[4])
-        currentOnOff = int(row[2])
-        # assign midi values
-        if currentOnOff == 1:
-            print(currentOnOff)
-            msg = mido.Message('note_on', note = currentPitch, velocity = currentVelocity, time = currentTime)
-            print(msg)
-            time.sleep(currentTime-previousTime)
-            print(msg.time)
-            port.send(msg)
-        elif currentOnOff == 0:
-            msg = mido.Message('note_off', note = currentPitch, velocity = currentVelocity, time = currentTime)
-            print(msg)
-            time.sleep(currentTime-previousTime)
-            print(msg.time)
-            port.send(msg)
-    time.sleep(3) # 3 sec pause
-    msg = mido.Message('note_on', note = 64, velocity = 1) # switch OFF "record" on Max/MSP
-    port.send(msg)
-
+                    #print(currentOnOff)
+                    track.append(mido.Message('note_off', note=currentPitch, velocity=currentVelocity, time=currentTime-previousTime))
+            mid.save(midname)
 
 
 # %%
