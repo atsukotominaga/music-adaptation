06b6bff0843e32b8d65ad7db395d81b981e9a3df
Atsuko Tominaga <Tominaga_Atsuko@phd.ceu.edu>
2020-08-08 15:32:10 +0200

    implement first part
    Signed-off-by: Atsuko Tominaga <Tominaga_Atsuko@phd.ceu.edu>

--- script/psychopy/rating.py
+++ script/psychopy/rating.py
@@ -1,19 +1,49 @@
-from psychopy import visual, core
+"""
+This script is based on tutorials on Psychopy3 website.
+"""
 
-# create a window
+# import libraries
+from psychopy import core, visual, gui, data, event
+from psychopy.tools.filetools import fromFile, toFile
+import numpy, random
 
-mywin = visual.Window([800,600], monitor="testMonitor", units="deg")
+# dialogue box
+expInfo = {'Number': '', 'Today': data.getDateStr()}
+dlg = gui.DlgFromDict(expInfo, fixed = ['Today'], title="Rating Pilot")
+if dlg.OK == False:
+    core.quit()
 
-# create some stimuli
-grating = visual.GratingStim(win=mywin, mask="circle", size=3, pos=[-4,0], sf=3)
-fixation = visual.GratingStim(win=mywin, size=0.5, pos=[0,0], sf=0, rgb=-1)
+"""
+   ('-.  ) (`-.       _ (`-.    ('-.  _  .-')          _   .-')       ('-.       .-') _  .-') _    
+ _(  OO)  ( OO ).    ( (OO  ) _(  OO)( \( -O )        ( '.( OO )_   _(  OO)     ( OO ) )(  OO) )   
+(,------.(_/.  \_)-._.`     \(,------.,------.  ,-.-') ,--.   ,--.)(,------.,--./ ,--,' /     '._  
+ |  .---' \  `.'  /(__...--'' |  .---'|   /`. ' |  |OO)|   `.'   |  |  .---'|   \ |  |\ |'--...__) 
+ |  |      \     /\ |  /  | | |  |    |  /  | | |  |  \|         |  |  |    |    \|  | )'--.  .--' 
+(|  '--.    \   \ | |  |_.' |(|  '--. |  |_.' | |  |(_/|  |'.'|  | (|  '--. |  .     |/    |  |    
+ |  .--'   .'    \_)|  .___.' |  .--' |  .  '.',|  |_.'|  |   |  |  |  .--' |  |\    |     |  |    
+ |  `---. /  .'.  \ |  |      |  `---.|  |\  \(_|  |   |  |   |  |  |  `---.|  | \   |     |  |    
+ `------''--'   '--'`--'      `------'`--' '--' `--'   `--'   `--'  `------'`--'  `--'     `--'    
 
-# draw the stimuli and update the window
-for frameN in range(200):
-    grating.setPhase(0.05, '+')
-    grating.draw()
-    fixation.draw()
-    mywin.update()
+"""
+# make a text file to save data
+filename = expInfo['Number'] + expInfo['Today']
+dataFile = open(filename + '.txt', 'w')
+dataFile.write('stimType, likertScale, RT\n')
 
-# pause for 5 secs
-core.wait(5.0)
+# create window and stimuli
+win = visual.Window([800, 600], fullscr = True, monitor = 'testMonitor', units = 'pix')
+fixation = visual.GratingStim(win, color = -1, colorSpace = 'rgb', tex=None, mask='circle', size=0.2)
+
+# add clocks
+globalClock = core.Clock()
+trialClock = core.Clock()
+
+# display instructions and wait
+mes1 = visual.TextStim(win, pos=[0, +3], font = 'Helvetica', height = 42, wrapWidth = 1400,
+    text='Thank you very much for taking part in the pilot study!\n\n In this experiment, you are going to listen to a number of piano performances and asked to rate to what extent musical expressions are implemented.')
+mes1.draw()
+fixation.draw()
+win.flip()
+
+# pause until there's a keypress
+event.waitKeys(keyList = ['space'])
