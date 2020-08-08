d826247e7150d72e36482972d085c10b9ccf5f96
Atsuko Tominaga <Tominaga_Atsuko@phd.ceu.edu>
2020-08-08 13:53:43 +0200

    test psychopy
    Signed-off-by: Atsuko Tominaga <Tominaga_Atsuko@phd.ceu.edu>

---
+++ script/psychopy/rating.py
@@ -0,0 +1,19 @@
+from psychopy import visual, core
+
+# create a window
+
+mywin = visual.Window([800,600], monitor="testMonitor", units="deg")
+
+# create some stimuli
+grating = visual.GratingStim(win=mywin, mask="circle", size=3, pos=[-4,0], sf=3)
+fixation = visual.GratingStim(win=mywin, size=0.5, pos=[0,0], sf=0, rgb=-1)
+
+# draw the stimuli and update the window
+for frameN in range(200):
+    grating.setPhase(0.05, '+')
+    grating.draw()
+    fixation.draw()
+    mywin.update()
+
+# pause for 5 secs
+core.wait(5.0)
