c6636bbf177a03007164927b1c18dfe09eb15516
Atsuko Tominaga <Tominaga_Atsuko@phd.ceu.edu>
2020-08-10 11:08:06 +0200

    implemented mid.play with mido
    Signed-off-by: Atsuko Tominaga <Tominaga_Atsuko@phd.ceu.edu>

--- script/psychopy/rating.py
+++ script/psychopy/rating.py
@@ -6,13 +6,17 @@ https://www.psychopy.org/coder/tutorial2.html
 """
 
 # import libraries
-from psychopy import core, visual, gui, data, event, prefs
-prefs.hardware['audioLib'] = ['PTB']
+from psychopy import core, visual, gui, data, event
 from psychopy import sound
 from psychopy.tools.filetools import fromFile, toFile
-import numpy, random
+import os, numpy, random, mido
 
-# dialogue box
+# open Max port
+# open max file
+os.system('open ' + "./midiplayer.maxpat")
+port = mido.open_output('to Max 1')
+
+# participant's info (only light mode)
 expInfo = {'Number': '', 'Today': data.getDateStr()}
 dlg = gui.DlgFromDict(expInfo, fixed = ['Today'], title="Rating Pilot")
 if dlg.OK == False:
@@ -36,7 +40,7 @@ dataFile = open(filename + '.txt', 'w')
 dataFile.write('stimType, likertScale, RT\n')
 
 # create window and stimuli
-win = visual.Window([800, 600], fullscr = True, monitor = 'testMonitor', units = 'pix')
+win = visual.Window([1920, 1200], fullscr = True, monitor = 'testMonitor', units = 'pix')
 fixation = visual.GratingStim(win, color = -1, colorSpace = 'rgb', tex=None, mask='circle', size=0.2)
 
 # add clocks
@@ -57,10 +61,13 @@ event.waitKeys(keyList = ['space'])
 
 # get response (rating scale)
 ratingScale = visual.RatingScale(win, low = 1, high = 5, markerStart = 3, leftKeys = '1', rightKeys = '2', acceptKeys = 'space', acceptPreText='move left (1) / right (2)')
+counter = 0
 while ratingScale.noResponse:
-    mid = sound.Sound('art_1.wav')
-    mid.setVolume(1)
-    mid.play(when=win)
+    mid = mido.MidiFile('../../material/stimuli/mid/art_1.mid')
+    if counter == 0:
+        for msg in mid.play():
+            port.send(msg)
+            counter += 1
     ratingScale.draw()
     win.flip()
 rating = ratingScale.getRating()
