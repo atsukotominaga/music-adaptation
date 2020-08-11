21736faf072a535a241dfc8e761ffd903935cbd2
Atsuko Tominaga <Tominaga_Atsuko@phd.ceu.edu>
2020-08-11 19:43:48 +0200

    implement basic structure
    Signed-off-by: Atsuko Tominaga <Tominaga_Atsuko@phd.ceu.edu>

--- script/psychopy/rating.py
+++ script/psychopy/rating.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 """
-This script is based on tutorials on Psychopy3 website.
-https://www.psychopy.org/coder/tutorial2.html
+This is a pre-experiment for stimuli selection.
+Code reference: https://www.psychopy.org/coder/tutorial2.html
 """
 
 # import libraries
@@ -45,14 +45,12 @@ def trial(imageFile, midFile, itemText, resultsList):
     item = visual.TextStim(win, pos=[0, 0], font = "Avenir", height = 60, wrapWidth = 1400,
     text = itemText)
     trialClock1 = core.Clock()
-    while ratingScale.noResponse:
+    while ratingScale.noResponse: # noResponse will be False once participant accepts the answer
         item.draw()
         ratingScale.draw()
         win.flip()
-    print(trialClock1.getTime())
-    print(ratingScale.getRating())
-    print(ratingScale.getRT())
-    print(ratingScale.getHistory())
+        
+    # store responses
     resultsList.append([
         ratingScale.getRating(), # final answer
         trialClock1.getTime(), # RT1
@@ -63,6 +61,7 @@ def trial(imageFile, midFile, itemText, resultsList):
     ])
     print(resultsList)
     event.clearEvents()
+    return
     
 #################
 
@@ -88,12 +87,13 @@ dlg = gui.DlgFromDict(expInfo, fixed = ["Today"], title="Rating Pilot")
 if dlg.OK == False:
     core.quit() # cancel
 
+
 # make a text file to save data
 if not os.path.exists("data"): # make a folder if not exists
     os.makedirs("data")
-filename = expInfo["Number"] + expInfo["Today"]
+""" filename = expInfo["Number"] + expInfo["Today"]
 dataFile = open("./data/" + filename + ".txt", "w")
-dataFile.write("stim, likertScale, RT\n")
+dataFile.write("stim, likertScale, RT\n") """
 
 # list to store answers
 answers = []
@@ -110,23 +110,88 @@ trialClock = core.Clock()
 ### Instrution ###
 # display instructions and wait
 ## instruction 1
-inst1 = visual.TextStim(win, pos=[0, 0], font = "Avenir", height = 60, wrapWidth = 1400,
-    text="Thank you very much for taking part in the pilot study!\n\n In this experiment, you are going to listen to a number of piano performances and asked to rate to what extent musical expressions are implemented.")
+inst1 = visual.TextStim(win, pos=[0, 0], font = "Avenir", height = 60, wrapWidth = 1400, alignText = "left",
+    text = "Thank you very much for participating in the pilot study!\n\nIn this experiment, you are going to listen to a number of piano performances and will be asked to rate to what extent musical expressions are implemented in each performance.\n\nPress <Space> to continue")
 inst1.draw()
 win.flip()
 next() # proceed/force quit
 
 ## instruction 2
+imageFile = visual.ImageStim(win, image = "./image/stim_m.png", pos = [0, -50], size = [1500, 535])
+inst2 = visual.TextStim(win, pos=[0, 0], font = "Avenir", height = 60, wrapWidth = 1400,
+    text = "You will listen to one piece with two notated musical techniques.\n\n\n\n\n\n\n\n\nPress <Space> to continue")
+imageFile.draw()
+inst2.draw()
+win.flip()
+next() # proceed/force quit
 
 ## instruction 3
+imageFile = visual.ImageStim(win, image = "./image/stim_m.png", pos = [0, -50], size = [1500, 535])
+inst3 = visual.TextStim(win, pos=[0, 0], font = "Avenir", height = 60, wrapWidth = 1400,
+    text = "[ Articulation ]\nthe smoothness of sound (legato and staccato)\n\n\n\n\n\n\n\n\nPress <Space> to continue")
+imageFile.draw()
+inst3.draw()
+win.flip()
+next() # proceed/force quit
+
+## instruction 4
+imageFile = visual.ImageStim(win, image = "./image/stim_m.png", pos = [0, -50], size = [1500, 535])
+inst4 = visual.TextStim(win, pos=[0, 0], font = "Avenir", height = 60, wrapWidth = 1400,
+    text = "[ Dynamics ]\nthe loudness of sound (forte and piano)\n\n\n\n\n\n\n\n\nPress <Space> to continue")
+imageFile.draw()
+inst4.draw()
+win.flip()
+next() # proceed/force quit
+
+## instruction 5
+inst5 = visual.TextStim(win, pos=[0, 0], font = "Avenir", height = 60, wrapWidth = 1400, alignText = "left",
+    text = "You will listen to a performance recording first. After that, you will be required to rate to what extent musical techniques are implemented in terms of articulation (legato and staccato) and dynamics (forte and piano).\n\nRating is separate for each technique.\n\nPress <Space> to continue")
+inst5.draw()
+win.flip()
+next() # proceed/force quit
+
+## instruction 6
+inst6 = visual.TextStim(win, pos=[0, 0], font = "Avenir", height = 60, wrapWidth = 1400, alignText = "left",
+    text = "Any quesions?\n\nThere will be 3 practice trials where you can see how to rate each performance. Also adjust the volume so that you can comfortably listen to the recordings.\n\nPress <Space> to start practice trials")
+inst6.draw()
+win.flip()
+next() # proceed/force quit
 
 ### Practice ###
-pImage = "./image/stim_m.png"
 pMid = "./mid/art_1.mid"
 pText = "Hello!"
-trial(pImage, pMid, pText, answers)
+# trial(imageFile, pMid, pText, answers)
+
+practice = True
+while practice:
+    for trial in range(3):
+        print(str(trial) + "trial")
+
+    ## instruction 7
+    inst7 = visual.TextStim(win, pos=[0, 0], font = "Avenir", height = 60, wrapWidth = 1400, alignText = "left",
+        text = "Any quesions?\n\nIf you want to repeat the practice trials,\nPress <Return> key.\n\nIf you are ready for experimental trials,\nPress <Space> to start")
+    inst7.draw()
+    win.flip()
+    resp = None
+    while resp == None:
+        allKeys = event.getKeys(keyList = ["space", "escape", "return"])
+        for resp in allKeys:
+            if resp == "space": # proceed
+                practice = False
+            elif resp == "escape": # force quit
+                core.quit()
+            elif resp == "returm": # practice again
+                practice = True
 
 ### Experiment ###
+inst8 = visual.TextStim(win, pos=[0, 0], font = "Avenir", height = 60, wrapWidth = 1400, alignText = "left",
+    text = "In the experimental, there are 64 performance recordings (i.e., trials).\n\nThe order of rating will be randomly presented (articulation first or dynamics first).\n\nAny questions? If not,\n\nPress <Space> to start experimental trials")
+inst8.draw()
+win.flip()
+next() # proceed/force quit
+
+for trial in range(64):
+    print(str(trial) + "trial")
 
 # write results
 for item in answers:
@@ -134,7 +199,7 @@ for item in answers:
         dataFile.close()
 
 ### Thank you ###
-thanks = visual.TextStim(win, pos=[0, 0], font = "Avenir", height = 60, wrapWidth = 1400,
+thanks = visual.TextStim(win, pos=[0, 0], font = "Avenir", height = 100, wrapWidth = 1400,
     text="Thank you!")
 thanks.draw()
 win.flip()
